703c25974c82af82b718c0ddb98e0acc1cfe1516
Ubuntu <ubuntu@ip-172-31-1-179.ap-south-1.compute.internal>
2023-08-31 10:48:05 +0000

    while loop added

---
+++ while-do.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Description: this is a basic while loop script, run for a certain number of seconds
+
+echo
+count=0
+num=10
+
+while [ $count -lt 10 ]; do
+    echo
+    echo "$num seconds left to stop this process $1"
+    echo
+    sleep 1
+    num=$(expr $num - 1)
+    count=$(expr $count + 1)
+done
+
+echo
+echo "$1 process is stopped"
+echo
+
